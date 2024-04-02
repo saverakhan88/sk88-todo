d0a2f89ca28c53aba28f1c04e5277eb85d7a5cbe
saverakhan88 <catsweet472@gmail.com>
2024-04-03 01:10:47 +0500

    Adding a TODO LIST APPLICATION

---
+++ main.ts
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+
+console.log(chalk.magenta.italic("\n\t Welcome to Savera's TODO LIST APPLICATION\n"));
+let condition = true;
+while(condition){
+let addTask = await inquirer.prompt(
+    [
+        {
+            name: "task", 
+            type: "input",
+            message: chalk.green("Enter your New Task:")
+
+        },
+        
+    ]
+
+);
+todoList.push(addTask.task);
+
+console.log(`${addTask.task} Task added in Todo List successfully`);
+
+
+let addMoreTask = await inquirer.prompt(
+    [
+        {
+            name: "addMore", 
+            type: "confirm",
+            message: chalk.grey("Do you want to add more Task?"),
+            default: "false"
+
+        },
+        
+    ]
+
+);
+condition = addMoreTask.addMore
+}
+
+console.log("Your updated TODO LIST:"  , todoList);
+
+
+
+
+
+
+
